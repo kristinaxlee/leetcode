52340d9a183a4bb9f469199afe25769986a28df7
kristinaxlee <50277029+kristinaxlee@users.noreply.github.com>
2020-07-06 18:30:42 -0700

    finished problems 35 & 509

---
+++ 35.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+
+    int binarySearch(vector<int>& nums, int target, int &index) {
+        int lo = 0;
+        int hi = nums.size()-1;
+        int mid;
+
+        while(lo <= hi) {
+            mid = (lo+hi)/2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+
+            else if(nums[mid] < target) {
+                lo = mid+1;
+            }
+
+            else {
+                hi = mid-1;
+            }
+        }
+
+        if(target < nums[lo]) {
+            index = lo;
+        }
+
+        else {
+            index = lo+1;
+        }
+
+        return index;
+
+    }
+
+    int searchInsert(vector<int>& nums, int target) {
+
+        if(nums[0] > target) {
+            return 0;
+        }
+
+        else if(nums[nums.size()-1] < target) {
+            return nums.size();
+        }
+
+        else {
+            int index;
+
+            return binarySearch(nums, target, index);
+        }
+
+    }
+};
---
+++ 509.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int N) {
+        if(N < 2) {
+            return N;
+        }
+        else {
+            return fib(N-1) + fib(N-2);
+        }
+
+    }
+};
