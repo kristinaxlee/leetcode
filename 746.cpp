6df18ac0f87ec71103b005d55b4b31d0a92afc18
kristinaxlee <50277029+kristinaxlee@users.noreply.github.com>
2020-07-09 21:32:21 -0700

    Create 746.cpp
    could be optimized to use less memory

---
+++ 746.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+
+        //int to keep track of the index we're on
+        int i = 0;
+        int total = 0;
+        int opt[cost.size()]; //create array to hold the optimum values
+
+        // if length of cost array is 2, return smaller element
+        if(cost.size() == 2) {
+            return min(cost[0], cost[1]);
+
+        }
+
+        // fill memoization array
+        for(int i=0; i < cost.size(); i++) {
+
+            if(i == 0 || i == 1) {
+                opt[i] = cost[i];
+            }
+
+            else {
+                opt[i] = min(opt[i-1] + cost[i], opt[i-2] + cost[i]);
+            }
+        }
+
+        // return the smaller of the last 2 elements in the array
+        return min(opt[cost.size()-1], opt[cost.size()-2]);
+    }
+};
